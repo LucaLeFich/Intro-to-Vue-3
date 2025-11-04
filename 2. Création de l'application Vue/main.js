20ac811edbb8edd2a8263de08dd80b75125456f2
LucaLeFich <pq48wkn@eduvaud.ch>
2025-11-04 15:24:51 +0100

    2. Création de l'application vue

---
+++ 2. Création de l'application Vue/main.js
@@ -0,0 +1,12 @@
+const product = 'Socks'
+const description = 'plip plop'
+
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Boots',
+
+            description: 'plip plop'
+        }
+    }
+})
